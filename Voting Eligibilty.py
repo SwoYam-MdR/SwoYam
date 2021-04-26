868de96557e770b02d66f821efd440c6da50cd69
SwoYam-MdR <83220018+SwoYam-MdR@users.noreply.github.com>
2021-04-26 18:30:06 +0545

    Add files via upload

---
+++ Voting Eligibilty.py
@@ -0,0 +1,8 @@
+print("What is your age")
+age=int(input())
+if age>18:
+    print("You can vote")
+elif age==18:
+    print("You need to complete some process then you can vote") # Extra step made by me 
+else:
+    print("You cannot vote")
