8e5ce0061d0b66e06650a9680c305eb8e5fbf079
SwoYam-MdR <83220018+SwoYam-MdR@users.noreply.github.com>
2021-04-26 19:02:00 +0545

    Add files via upload

---
+++ dictioneryy.py
@@ -0,0 +1,13 @@
+#Dictionary
+
+d1 = {"benefit":"to get advantage", "violent":"to violate others", "vanish":"disappear", "humilate":"make someone foolish by injuring their dignity or pride"}
+print(d1[input()])
+print(d1[input()])
+print(d1[input()])
+print(d1[input()])
+print(d1[input()])
+print(d1[input()])
+print(d1[input()])
+print(d1[input()])
+print(d1[input()])
+print(d1[input()])
