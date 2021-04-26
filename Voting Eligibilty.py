d2c71c8100c36fa67031562383c66dd43e8373a4
SwoYam-MdR <83220018+SwoYam-MdR@users.noreply.github.com>
2021-04-26 18:51:03 +0545

    Update Voting Eligibilty.py

--- Voting Eligibilty.py
+++ Voting Eligibilty.py
@@ -1,12 +1,8 @@
 print("What is your age")
 age=int(input())
-if age>>110:
+if age>18:
     print("You can vote")
 elif age==18:
     print("You need to complete some process then you can vote") # Extra step made by me
-elif age<3:
-    print("Wtf its not possible you are not supposed to be here") #lol
-elif age>110:
-    print("wtf you should be dead ,,,,thinkss ,,,m**f Runnnnn")
 else:
     print("You cannot vote")
